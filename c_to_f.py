a827719261f644ebe4d8c223899949848895e1c8
aaron0096 <109711683+aaron0096@users.noreply.github.com>
2022-08-29 11:51:41 +0800

    Update c_to_f.py

--- c_to_f.py
+++ c_to_f.py
@@ -17,6 +17,11 @@ def fahrenheit_from(celsius):
 
 @app.route("/")
 def index():
+    app.logger.debug("debug log info")
+    app.logger.info("Info log information")
+    app.logger.warning("Warning log info")
+    app.logger.error("Error log info")
+    app.logger.critical("Critical log info")
     celsius = request.args.get("celsius", "")
     if celsius:
         fahrenheit = fahrenheit_from(celsius)
@@ -30,11 +35,7 @@ def index():
         + "Fahrenheit: "
         + fahrenheit
     )
-    app.logger.debug("debug log info")
-    app.logger.info("Info log information")
-    app.logger.warning("Warning log info")
-    app.logger.error("Error log info")
-    app.logger.critical("Critical log info")
+
 
 
 if __name__ == "__main__":
