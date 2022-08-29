a3183cc5e5ce3962047f5fec7c6d5b2bde30a1ea
aaron0096 <109711683+aaron0096@users.noreply.github.com>
2022-08-29 12:06:36 +0800

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import logging
+
+logging.basicConfig(filename="record_newapp.log", level=logging.DEBUG, format="{asctime}, {levelname}, {name}, {message}".format())
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    app.logger.debug("debug log info")
+    app.logger.info("Info log information")
+    app.logger.warning("Warning log info")
     return 'Hello, Docker!'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=9000, debug=True)
